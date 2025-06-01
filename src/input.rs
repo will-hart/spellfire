26c2fd3109b7ccd907de64d8c8faa0d20a9f0ed8
William Hart <391950+will-hart@users.noreply.github.com>
2025-06-02 07:55:03 +1000

    Support camera zoom and pan
    Signed-off-by: William Hart <391950+will-hart@users.noreply.github.com>

--- src/input.rs
+++ src/input.rs
@@ -4,11 +4,15 @@ use bevy::{prelude::*, window::PrimaryWindow};
 
 use crate::MainCamera;
 
+mod gestures;
+
 pub(super) fn plugin(app: &mut App) {
     app.register_type::<MousePosition>();
 
     app.add_systems(Startup, setup_mouse_tracking);
     app.add_systems(PreUpdate, track_mouse);
+
+    app.add_plugins(gestures::plugin);
 }
 
 /// Tracks the current position of the mouse
@@ -17,6 +21,9 @@ pub(super) fn plugin(app: &mut App) {
 pub struct MousePosition {
     pub viewport_pos: Vec2,
     pub world_pos: Vec2,
+
+    pub primary_pressed: bool,
+    pub viewport_delta: Vec2,
 }
 
 fn setup_mouse_tracking(mut commands: Commands) {
@@ -25,16 +32,22 @@ fn setup_mouse_tracking(mut commands: Commands) {
 
 fn track_mouse(
     mut history: ResMut<MousePosition>,
+    buttons: Res<ButtonInput<MouseButton>>,
     window: Single<&Window, With<PrimaryWindow>>,
     camera: Single<(&Camera, &GlobalTransform), With<MainCamera>>,
 ) {
     let (camera, camera_transform) = *camera;
+    let prev_viewport = history.viewport_pos;
+    history.primary_pressed = buttons.pressed(MouseButton::Left);
 
     if let Some(cursor) = window.cursor_position() {
         history.viewport_pos = cursor;
+        history.viewport_delta = prev_viewport - cursor;
 
         if let Ok(ray) = camera.viewport_to_world(camera_transform, cursor) {
             history.world_pos = ray.origin.truncate();
         }
+    } else {
+        history.viewport_delta = Vec2::ZERO;
     }
 }
